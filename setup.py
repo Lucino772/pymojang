508a389212cd819ab73eaf5aaac3daefc1b00828
Lucino772 <lucapalmi772@gmail.com>
2021-03-08 21:49:50 +0100

    Python package setup + dependencies

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='pymojang',
+    version='0.1',
+    author='Lucino772',
+    author_email='lucapalmi772@gmail.com',
+    licence='MIT',
+    description='It\'s a full wrapper arround de mojang API and authentication API',
+    packages=['pymojang'],
+    install_requires=['requests']
+)
