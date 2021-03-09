d562b9a111c8367556746476dc50229aedcad7d3
Lucino772 <lucapalmi772@gmail.com>
2021-03-09 17:54:01 +0100

    Mojang API Wrapper with functions and classes

---
+++ pymojang/user/profile.py
@@ -0,0 +1,13 @@
+
+class UserProfile:
+
+    def __init__(self):
+        self.created_at = None
+        self.name_change_allowed = None
+
+        self.id = None
+        self.name = None
+        self.skins = []
+        self.capes = []
+
+        self.names = []
