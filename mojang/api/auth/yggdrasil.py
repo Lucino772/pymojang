609ce40e0c0d883771fcc63c1858303995d797e6
Lucino772 <lucapalmi772@gmail.com>
2021-03-24 23:14:53 +0100

    Documentation for mojang.api.auth.yggdrasil

--- mojang/api/auth/yggdrasil.py
+++ mojang/api/auth/yggdrasil.py
@@ -8,6 +8,21 @@ from ..urls import AUTHENTICATE, INVALIDATE, REFRESH, SIGNOUT, VALIDATE
 
 
 def authenticate_user(username: str, password: str, client_token=None) -> dict:
+    """Authenticate user with name and password
+
+    Args:
+        username (str): The username or email if account is not legacy
+        password (str): The user password
+        client_token (str, optional): The client token to use in the authentication (default to None)
+
+    Returns:
+        A dict with thet following keys : `access_token`, `client_token`, `uuid`, `name`, 
+        `legacy` and `demo` 
+
+    Raises:
+        CredentialsError: If username and password are invalid
+        PayloadError: If credentials are not formated correctly
+    """
     payload = {
         'username': username,
         'password': password,
@@ -29,7 +44,21 @@ def authenticate_user(username: str, password: str, client_token=None) -> dict:
         'demo': not data['selectedProfile'].get('paid', True)
     }
 
-def refresh_token(access_token: str, client_token: str):
+def refresh_token(access_token: str, client_token: str) -> dict:
+    """Refresh an invalid access token
+
+    Args:
+        access_token (str): The access token to refresh
+        clien_token (str): The client token used to generate the access token
+
+    Returns:
+        A dict with the following keys: `access_token`, `client_token`, `uuid`, `name`,
+        `legacy` and `demo` 
+
+    Raises:
+        TokenError: If client token is not the one used to generate the access token
+        PayloadError: If the tokens are not formated correctly
+    """
     payload = {
         'accessToken': access_token,
         'clientToken': client_token
@@ -47,6 +76,16 @@ def refresh_token(access_token: str, client_token: str):
     }
 
 def validate_token(access_token: str, client_token: str):
+    """Validate an access token
+
+    Args:
+        access_token (str): The access token to validate
+        clien_token (str): The client token used to generate the access token
+
+    Raises:
+        TokenError: If client token is not the one used to generate the access token
+        PayloadError: If the tokens are not formated correctly
+    """
     payload = {
         'accessToken': access_token,
         'clientToken': client_token
@@ -55,6 +94,16 @@ def validate_token(access_token: str, client_token: str):
     web.request('post', VALIDATE, exceptions=(PayloadError, TokenError), json=payload)
 
 def signout_user(username: str, password: str):
+    """Signout user with name and password
+
+    Args:
+        username (str): The username or email if account is not legacy
+        password (str): The user password
+
+    Raises:
+        CredentialsError: If username and password are invalid
+        PayloadError: If credentials are not formated correctly
+    """
     payload = {
         'username': ctx.username,
         'password': ctx.password
@@ -63,6 +112,16 @@ def signout_user(username: str, password: str):
     web.request('post', SIGNOUT, exceptions=(PayloadError, CredentialsError), json=payload)
 
 def invalidate_token(access_token: str, client_token: str):
+    """Invalidate an access token
+
+    Args:
+        access_token (str): The access token to invalidate
+        clien_token (str): The client token used to generate the access token
+
+    Raises:
+        TokenError: If client token is not the one used to generate the access token
+        PayloadError: If the tokens are not formated correctly
+    """
     payload = {
         'accessToken': access_token,
         'clientToken': client_token
