e65b11adb1675499a8d4921611f32306f864816d
Lucino772 <lucapalmi772@gmail.com>
2021-03-24 04:53:49 +0100

    Added urls and description to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 import setuptools
 
+with open('./README.md','r') as fp:
+    long_description = fp.read()
+
 setuptools.setup(
     name='pymojang',
     version='1.0',
     author='Lucino772',
     author_email='lucapalmi772@gmail.com',
     licence='MIT',
+    url='https://github.com/Lucino772/pymojang',
     description='It\'s a full wrapper arround de Mojang API and Mojang Authentication API',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     packages=['mojang'],
-    install_requires=['requests','validators']
+    install_requires=['requests','validators'],
+    keywords=['minecraft','mojang','python3']
+    project_urls={
+        'Source': 'https://github.com/Lucino772/pymojang',
+        'Documentation': 'https://pymojang.readthedocs.io/en/latest/'
+    }
 )
