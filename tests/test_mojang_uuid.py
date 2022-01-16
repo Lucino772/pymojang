a53c96c0c64ec39fa01dc6e8895735e9e5744b04
Lucino772 <lucapalmi772@gmail.com>
2022-01-16 17:09:40 +0100

    test: PYM-18 Added tests for mojang.get_uuid

---
+++ tests/test_mojang_uuid.py
@@ -0,0 +1,22 @@
+import unittest
+
+import mojang
+from mojang.exceptions import InvalidName
+
+
+class TestMojangStatus(unittest.TestCase):
+    def setUp(self) -> None:
+        self.notch = mojang.get_uuid("Notch")
+        self.jeb_ = mojang.get_uuid("jeb_")
+        self.unkown = mojang.get_uuid("UNEXISTENTPLAYER")
+
+    def test_existent_uuid(self):
+        self.assertEqual(self.notch.uuid, "069a79f444e94726a5befca90e38aaf5")
+        self.assertEqual(self.jeb_.uuid, "853c80ef3c3749fdaa49938b674adae6")
+
+    def test_unexistent_uuid(self):
+        self.assertEqual(self.unkown, None)
+
+    def test_invalid_uuid(self):
+        self.assertRaises(InvalidName, mojang.get_uuid, "")
+        self.assertRaises(InvalidName, mojang.get_uuid, "xxxxxxxxxxxxxxxxx")
