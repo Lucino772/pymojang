46b5df07d035dd6b338a9d697584b0aedd829f7c
Lucino772 <lucapalmi772@gmail.com>
2022-01-19 18:04:21 +0100

    test(tests/): pYM-18 Added tests for mojang.account.session.change_user_skin

---
+++ tests/session/test_mojang_change_skin.py
@@ -0,0 +1,31 @@
+import unittest
+from unittest import mock
+from mojang.account import session
+from mojang.exceptions import Unauthorized
+from tests.session.mock_server import MockSessionServer
+
+VALID_ACCESS_TOKEN = "MY_ACCESS_TOKEN"
+INVALID_ACCESS_TOKEN = "NOT_MY_ACCESS_TOKEN"
+
+mock_server = MockSessionServer(
+    VALID_ACCESS_TOKEN, unavailable_names=["Notch"]
+)
+
+
+class TestMojangChangeSkin(unittest.TestCase):
+    @mock.patch("requests.post", side_effect=mock_server.change_user_skin)
+    def test_valid_token(self, mock_post: mock.MagicMock):
+        ok = session.change_user_skin(
+            VALID_ACCESS_TOKEN,
+            "http://textures.minecraft.net/texture/292009a4925b58f02c77dadc3ecef07ea4c7472f64e0fdc32ce5522489362680",
+        )
+        self.assertEqual(ok, True)
+
+    @mock.patch("requests.post", side_effect=mock_server.change_user_skin)
+    def test_invalid_token(self, mock_post: mock.MagicMock):
+        self.assertRaises(
+            Unauthorized,
+            session.change_user_skin,
+            INVALID_ACCESS_TOKEN,
+            "http://textures.minecraft.net/texture/292009a4925b58f02c77dadc3ecef07ea4c7472f64e0fdc32ce5522489362680",
+        )
