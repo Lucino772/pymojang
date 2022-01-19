29aa935458e4b9a540633a2d6c1c9998774b8cfc
Lucino772 <lucapalmi772@gmail.com>
2022-01-19 18:01:01 +0100

    test(tests/): pYM-18 Added tests fro mojang.account.session.change_user_name

---
+++ tests/session/test_mojang_change_name.py
@@ -0,0 +1,46 @@
+import unittest
+from unittest import mock
+from mojang.account import session
+from mojang.exceptions import InvalidName, UnavailableName, Unauthorized
+from tests.session.mock_server import MockSessionServer
+
+VALID_ACCESS_TOKEN = "MY_ACCESS_TOKEN"
+INVALID_ACCESS_TOKEN = "NOT_MY_ACCESS_TOKEN"
+
+mock_server = MockSessionServer(
+    VALID_ACCESS_TOKEN, unavailable_names=["Notch"]
+)
+
+
+class TestMojangChangeName(unittest.TestCase):
+    @mock.patch("requests.put", side_effect=mock_server.change_user_name)
+    def test_valid_token(self, mock_put: mock.MagicMock):
+        ok = session.change_user_name(VALID_ACCESS_TOKEN, "lucino")
+        self.assertEqual(ok, True)
+
+    @mock.patch("requests.put", side_effect=mock_server.change_user_name)
+    def test_invalid_token(self, mock_put: mock.MagicMock):
+        self.assertRaises(
+            Unauthorized,
+            session.change_user_name,
+            INVALID_ACCESS_TOKEN,
+            "lucino",
+        )
+
+    @mock.patch("requests.put", side_effect=mock_server.change_user_name)
+    def test_invalid_name(self, mock_put: mock.MagicMock):
+        self.assertRaises(
+            InvalidName,
+            session.change_user_name,
+            VALID_ACCESS_TOKEN,
+            "xxxxxxxxxxxxxxxxx",
+        )
+
+    @mock.patch("requests.put", side_effect=mock_server.change_user_name)
+    def test_unavailable_name(self, mock_put: mock.MagicMock):
+        self.assertRaises(
+            UnavailableName,
+            session.change_user_name,
+            VALID_ACCESS_TOKEN,
+            "Notch",
+        )
