858285a86218b4bb99b5d1473fc90d378edb8c5d
Lucino772 <lucapalmi772@gmail.com>
2022-01-19 17:58:35 +0100

    test(tests/): pYM-18 Added tests for mojang.account.session.get_user_name_change

---
+++ tests/session/test_mojang_name_change.py
@@ -0,0 +1,30 @@
+import datetime as dt
+import unittest
+from unittest import mock
+
+from mojang.account import session
+from mojang.exceptions import Unauthorized
+
+from .mock_server import MockSessionServer
+
+VALID_ACCESS_TOKEN = "MY_ACCESS_TOKEN"
+INVALID_ACCESS_TOKEN = "NOT_MY_ACCESS_TOKEN"
+
+mock_server = MockSessionServer(VALID_ACCESS_TOKEN)
+
+
+class TestMojangNameChange(unittest.TestCase):
+    @mock.patch("requests.get", side_effect=mock_server.user_name_change)
+    def test_valid_token(self, mock_get: mock.MagicMock):
+        res = session.get_user_name_change(VALID_ACCESS_TOKEN)
+        self.assertEqual(res.allowed, True)
+        self.assertEqual(
+            res.created_at,
+            dt.datetime.strptime("2021-01-01T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ"),
+        )
+
+    @mock.patch("requests.get", side_effect=mock_server.user_name_change)
+    def test_invalid_token(self, mock_get: mock.MagicMock):
+        self.assertRaises(
+            Unauthorized, session.get_user_name_change, INVALID_ACCESS_TOKEN
+        )
