4b44114094d260ccb9748b052e25103d294a6a35
Lucino772 <lucapalmi772@gmail.com>
2022-05-05 14:51:16 +0200

    test(minecraft): added test cases for rcon & query

---
+++ tests/minecraft/test_proto_query.py
@@ -0,0 +1,7 @@
+import unittest
+
+from mojang.minecraft import query
+
+
+class TestProtoQuery(unittest.TestCase):
+    pass  # TODO: Write tests for Query protocol
---
+++ tests/minecraft/test_proto_rcon.py
@@ -0,0 +1,7 @@
+import unittest
+
+from mojang.minecraft import rcon
+
+
+class TestProtoRcon(unittest.TestCase):
+    pass  # TODO: Write tests for RCON protocol
