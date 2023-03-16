7a1599d21aef8308afdfd5e76257b9d8ec0474af
Lucino772 <lucapalmi772@gmail.com>
2023-03-16 12:37:02 +0100

    test: mockin tests for `get_profile`

--- tests/test_mojang_user.py
+++ tests/test_mojang_user.py
@@ -1,52 +1,104 @@
 import unittest
 
+import responses
+
 import mojang
-from mojang.api.models import Cape, Skin
-from mojang.api.structures import UnauthenticatedProfile
-from mojang.exceptions import InvalidName
+from mojang.api.urls import api_user_profile
+from mojang.exceptions import MethodNotAllowed, NotFound, ServerError
 
 
 class TestMojangStatus(unittest.TestCase):
-    def setUp(self) -> None:
-        self.notch = mojang.get_profile("069a79f444e94726a5befca90e38aaf5")
-        self.jeb_ = mojang.get_profile("853c80ef3c3749fdaa49938b674adae6")
-        self.unkown = mojang.get_profile("069a79f444e94726a5befca90e38aaf6")
-
-    def test_existent_user(self):
-        self.assertEqual(
-            self.notch,
-            UnauthenticatedProfile(
-                "Notch",
-                "069a79f444e94726a5befca90e38aaf5",
-                False,
-                False,
-                Skin(
-                    "http://textures.minecraft.net/texture/292009a4925b58f02c77dadc3ecef07ea4c7472f64e0fdc32ce5522489362680",
-                    "classic",
-                ),
-                None,
-            ),
+    @responses.activate
+    def test200(self):
+        # TODO: Test With & Without Cape
+        uuid = "069a79f444e94726a5befca90e38aaf5"  # Notch
+        responses.add(
+            method=responses.GET,
+            url=api_user_profile(uuid),
+            json={
+                "id": uuid,
+                "name": "Notch",
+                "properties": [
+                    {
+                        "name": "textures",
+                        "value": (
+                            "ewogICJ0aW1lc3RhbXAiIDogMTY3ODk2NDQ0MzQ4NSw"
+                            "KICAicHJvZmlsZUlkIiA6ICIwNjlhNzlmNDQ0ZTk0Nz"
+                            "I2YTViZWZjYTkwZTM4YWFmNSIsCiAgInByb2ZpbGVOY"
+                            "W1lIiA6ICJOb3RjaCIsCiAgInRleHR1cmVzIiA6IHsK"
+                            "ICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR"
+                            "0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dH"
+                            "VyZS8yOTIwMDlhNDkyNWI1OGYwMmM3N2RhZGMzZWNlZ"
+                            "jA3ZWE0Yzc0NzJmNjRlMGZkYzMyY2U1NTIyNDg5MzYy"
+                            "NjgwIgogICAgfQogIH0KfQ=="
+                        ),
+                    }
+                ],
+            },
+            status=200,
+        )
+        responses.add(
+            method=responses.GET,
+            url="http://textures.minecraft.net/texture/292009a4925b58f02c77dadc3ecef07ea4c7472f64e0fdc32ce5522489362680",
+            body=b"",
+            status=200,
         )
 
+        profile = mojang.get_profile(uuid)
+        self.assertEqual(profile.name, "Notch")
+        self.assertEqual(profile.uuid, uuid)
+        self.assertEqual(profile.is_legacy, False)
+        self.assertEqual(profile.is_demo, False)
+        self.assertIsNotNone(profile.skin)
         self.assertEqual(
-            self.jeb_,
-            UnauthenticatedProfile(
-                "jeb_",
-                "853c80ef3c3749fdaa49938b674adae6",
-                False,
-                False,
-                Skin(
-                    "http://textures.minecraft.net/texture/7fd9ba42a7c81eeea22f1524271ae85a8e045ce0af5a6ae16c6406ae917e68b5",
-                    "classic",
-                ),
-                Cape(
-                    "http://textures.minecraft.net/texture/9e507afc56359978a3eb3e32367042b853cddd0995d17d0da995662913fb00f7"
-                ),
-            ),
+            profile.skin.source,
+            "http://textures.minecraft.net/texture/292009a4925b58f02c77dadc3ecef07ea4c7472f64e0fdc32ce5522489362680",
+        )
+        self.assertEqual(profile.skin.variant, "classic")
+        self.assertIsNone(profile.cape)
+
+    @responses.activate
+    def test204(self):
+        uuid = "069a79f444e94726a5befca90e38aaf6"  # Does not exists
+        responses.add(
+            method=responses.GET, url=api_user_profile(uuid), status=204
         )
 
-    def test_unexistent_user(self):
-        self.assertEqual(self.unkown, None)
+        profile = mojang.get_profile(uuid)
+        self.assertIsNone(profile)
 
-    def test_invalid_user(self):
-        self.assertRaises(ValueError, mojang.get_profile, "thisisnotauuid")
+    @responses.activate
+    def test400(self):
+        uuid = "thisisnotauuid"
+        responses.add(
+            method=responses.GET, url=api_user_profile(uuid), status=400
+        )
+
+        self.assertRaises(ValueError, mojang.get_profile, uuid)
+
+    @responses.activate
+    def test404(self):
+        uuid = "069a79f444e94726a5befca90e38aaf6"  # Does not exists
+        responses.add(
+            method=responses.GET, url=api_user_profile(uuid), status=404
+        )
+
+        self.assertRaises(NotFound, mojang.get_profile, uuid)
+
+    @responses.activate
+    def test405(self):
+        uuid = "069a79f444e94726a5befca90e38aaf6"  # Does not exists
+        responses.add(
+            method=responses.GET, url=api_user_profile(uuid), status=405
+        )
+
+        self.assertRaises(MethodNotAllowed, mojang.get_profile, uuid)
+
+    @responses.activate
+    def test500(self):
+        uuid = "069a79f444e94726a5befca90e38aaf6"  # Does not exists
+        responses.add(
+            method=responses.GET, url=api_user_profile(uuid), status=500
+        )
+
+        self.assertRaises(ServerError, mojang.get_profile, uuid)
