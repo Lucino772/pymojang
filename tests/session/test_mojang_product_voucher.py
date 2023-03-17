0c86d184962ff0a0b55d17cfe9caa8b42894a06e
Lucino772 <lucapalmi772@gmail.com>
2023-03-17 01:37:22 +0100

    test: mocking tests for product vouchers

--- tests/session/test_mojang_product_voucher.py
+++ tests/session/test_mojang_product_voucher.py
@@ -1,81 +1,151 @@
-import datetime as dt
 import unittest
-from unittest import mock
+
+import responses
 
 from mojang.api import session
+from mojang.api.urls import api_session_product_voucher
 from mojang.exceptions import Unauthorized
-
-from .mock_server import MockSessionServer
-
-VALID_ACCESS_TOKEN = "MY_ACCESS_TOKEN"
-INVALID_ACCESS_TOKEN = "NOT_MY_ACCESS_TOKEN"
-
-mock_server = MockSessionServer(
-    VALID_ACCESS_TOKEN,
-    valid_vouchers=["8901854197", "JHRD2-HWGTY-WP3MW-QR4MC-CGGHZ"],
-    used_vouchers=["JHRD2-HWGTY-WP3MW-QR4MC-CGGHZ"],
-)
 
 
 class TestMojangCheckProductVoucher(unittest.TestCase):
-    @mock.patch("requests.get", side_effect=mock_server.product_voucher)
-    def test_invalid_voucher(self, mock_get: mock.MagicMock):
+    @responses.activate
+    def test200(self):
+        voucher = "12345"
+        responses.add(
+            method=responses.GET,
+            url=api_session_product_voucher(voucher),
+            status=200,
+            json={
+                "voucherInfo": {
+                    "code": "00000-00000-00000-00000-00000",
+                    "productVariant": "minecraft",
+                    "status": "ACTIVE",
+                },
+                "productRedeemable": True,
+            },
+        )
+
+        available = session.check_product_voucher("TOKEN", voucher)
+        self.assertTrue(available)
+
+    @responses.activate
+    def test401(self):
+        voucher = "12345"
+        responses.add(
+            method=responses.GET,
+            url=api_session_product_voucher(voucher),
+            status=401,
+        )
+
         self.assertRaises(
-            ValueError,
-            session.check_product_voucher,
-            VALID_ACCESS_TOKEN,
-            "12345",
+            Unauthorized, session.check_product_voucher, "TOKEN", voucher
         )
 
-    @mock.patch("requests.get", side_effect=mock_server.product_voucher)
-    def test_used_voucher(self, mock_get: mock.MagicMock):
-        res = session.check_product_voucher(
-            VALID_ACCESS_TOKEN, "JHRD2-HWGTY-WP3MW-QR4MC-CGGHZ"
+    @responses.activate
+    def test404_invalid(self):
+        voucher = "12345"
+        responses.add(
+            method=responses.GET,
+            url=api_session_product_voucher(voucher),
+            status=404,
+            json={
+                "path": "/productvoucher/:voucher",
+                "errorType": "NOT_FOUND",
+                "error": "NOT_FOUND",
+            },
         )
-        self.assertEqual(res, False)
 
-    @mock.patch("requests.get", side_effect=mock_server.product_voucher)
-    def test_unused_voucher(self, mock_get: mock.MagicMock):
-        res = session.check_product_voucher(VALID_ACCESS_TOKEN, "8901854197")
-        self.assertEqual(res, True)
-
-    @mock.patch("requests.get", side_effect=mock_server.product_voucher)
-    def test_invalid_token(self, mock_get: mock.MagicMock):
         self.assertRaises(
-            Unauthorized,
-            session.check_product_voucher,
-            INVALID_ACCESS_TOKEN,
-            "8901854197",
+            ValueError, session.check_product_voucher, "TOKEN", voucher
         )
+
+    @responses.activate
+    def test404_unavailable(self):
+        voucher = "12345"
+        responses.add(
+            method=responses.GET,
+            url=api_session_product_voucher(voucher),
+            status=404,
+            json={
+                "path": "/productvoucher/:voucher",
+                "errorType": "NOT_FOUND",
+                "error": "NOT_FOUND",
+                "errorMessage": "The server has not found anything matching the request URI",
+                "developerMessage": "The server has not found anything matching the request URI",
+            },
+        )
+
+        available = session.check_product_voucher("TOKEN", voucher)
+        self.assertFalse(available)
 
 
 class TestMojangRedeemProductVoucher(unittest.TestCase):
-    @mock.patch("requests.put", side_effect=mock_server.product_voucher)
-    def test_invalid_voucher(self, mock_put: mock.MagicMock):
+    @responses.activate
+    def test200(self):
+        voucher = "12345"
+        responses.add(
+            method=responses.PUT,
+            url=api_session_product_voucher(voucher),
+            status=200,
+            json={
+                "voucherInfo": {
+                    "code": "00000-00000-00000-00000-00000",
+                    "productVariant": "minecraft",
+                    "status": "ACTIVE",
+                },
+                "productRedeemable": True,
+            },
+        )
+
+        available = session.redeem_product_voucher("TOKEN", voucher)
+        self.assertTrue(available)
+
+    @responses.activate
+    def test401(self):
+        voucher = "12345"
+        responses.add(
+            method=responses.PUT,
+            url=api_session_product_voucher(voucher),
+            status=401,
+        )
+
         self.assertRaises(
-            ValueError,
-            session.redeem_product_voucher,
-            VALID_ACCESS_TOKEN,
-            "12345",
+            Unauthorized, session.redeem_product_voucher, "TOKEN", voucher
         )
 
-    @mock.patch("requests.put", side_effect=mock_server.product_voucher)
-    def test_used_voucher(self, mock_put: mock.MagicMock):
-        res = session.redeem_product_voucher(
-            VALID_ACCESS_TOKEN, "JHRD2-HWGTY-WP3MW-QR4MC-CGGHZ"
+    @responses.activate
+    def test404_invalid(self):
+        voucher = "12345"
+        responses.add(
+            method=responses.PUT,
+            url=api_session_product_voucher(voucher),
+            status=404,
+            json={
+                "path": "/productvoucher/:voucher",
+                "errorType": "NOT_FOUND",
+                "error": "NOT_FOUND",
+            },
         )
-        self.assertEqual(res, False)
 
-    @mock.patch("requests.put", side_effect=mock_server.product_voucher)
-    def test_unused_voucher(self, mock_put: mock.MagicMock):
-        res = session.redeem_product_voucher(VALID_ACCESS_TOKEN, "8901854197")
-        self.assertEqual(res, True)
-
-    @mock.patch("requests.put", side_effect=mock_server.product_voucher)
-    def test_invalid_token(self, mock_put: mock.MagicMock):
         self.assertRaises(
-            Unauthorized,
-            session.redeem_product_voucher,
-            INVALID_ACCESS_TOKEN,
-            "8901854197",
+            ValueError, session.redeem_product_voucher, "TOKEN", voucher
         )
+
+    @responses.activate
+    def test404_unavailable(self):
+        voucher = "12345"
+        responses.add(
+            method=responses.PUT,
+            url=api_session_product_voucher(voucher),
+            status=404,
+            json={
+                "path": "/productvoucher/:voucher",
+                "errorType": "NOT_FOUND",
+                "error": "NOT_FOUND",
+                "errorMessage": "The server has not found anything matching the request URI",
+                "developerMessage": "The server has not found anything matching the request URI",
+            },
+        )
+
+        available = session.redeem_product_voucher("TOKEN", voucher)
+        self.assertFalse(available)
