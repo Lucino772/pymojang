cf762fee4d486f2cae3be422fae5e657331e64ae
Lucino772 <lucapalmi772@gmail.com>
2023-03-17 01:13:12 +0100

    test: mocking tests for `hide_user_cape`

--- tests/session/test_mojang_hide_cape.py
+++ tests/session/test_mojang_hide_cape.py
@@ -1,24 +1,30 @@
 import unittest
-from unittest import mock
+
+import responses
 
 from mojang.api import session
+from mojang.api.urls import api_session_cape_visibility
 from mojang.exceptions import Unauthorized
-from tests.session.mock_server import MockSessionServer
-
-VALID_ACCESS_TOKEN = "MY_ACCESS_TOKEN"
-INVALID_ACCESS_TOKEN = "NOT_MY_ACCESS_TOKEN"
-
-mock_server = MockSessionServer(VALID_ACCESS_TOKEN)
 
 
 class TestMojangHideCape(unittest.TestCase):
-    @mock.patch("requests.delete", side_effect=mock_server.hide_user_cape)
-    def test_valid_token(self, mock_delete: mock.MagicMock):
-        ok = session.hide_user_cape(VALID_ACCESS_TOKEN)
-        self.assertEqual(ok, True)
-
-    @mock.patch("requests.delete", side_effect=mock_server.hide_user_cape)
-    def test_invalid_token(self, mock_delete: mock.MagicMock):
-        self.assertRaises(
-            Unauthorized, session.hide_user_cape, INVALID_ACCESS_TOKEN
+    @responses.activate
+    def test200(self):
+        responses.add(
+            method=responses.DELETE,
+            url=api_session_cape_visibility,
+            status=200,
         )
+
+        hidden = session.hide_user_cape("TOKEN")
+        self.assertTrue(hidden)
+
+    @responses.activate
+    def test401(self):
+        responses.add(
+            method=responses.DELETE,
+            url=api_session_cape_visibility,
+            status=401,
+        )
+
+        self.assertRaises(Unauthorized, session.hide_user_cape, "TOKEN")
