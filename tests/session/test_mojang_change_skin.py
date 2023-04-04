0e032015ccd207c0b3c43d7f4da4640cb30a49ca
Lucino772 <lucapalmi772@gmail.com>
2023-04-04 23:00:31 +0200

    test: improved tests for `change_user_skin`

--- tests/session/test_mojang_change_skin.py
+++ tests/session/test_mojang_change_skin.py
@@ -1,8 +1,10 @@
 import unittest
 
 import responses
+from responses.matchers import multipart_matcher
 
 from mojang.api import session
+from mojang.api.models import Skin
 from mojang.api.urls import api_session_change_skin
 from mojang.exceptions import Unauthorized
 
@@ -15,8 +17,18 @@ class TestMojangChangeSkin(unittest.TestCase):
     def test204(self):
         skin_url = "http://textures.minecraft.net/texture/292009a4925b58f02c77dadc3ecef07ea4c7472f64e0fdc32ce5522489362680"
         self._path_skin_url(skin_url)
+        skin = Skin(source=skin_url, variant="classic")
+
         responses.add(
-            method=responses.POST, url=api_session_change_skin, status=204
+            method=responses.POST,
+            url=api_session_change_skin,
+            status=204,
+            match=[
+                multipart_matcher(
+                    {"file": ("image.png", skin.data, "image/png")},
+                    data={"variant": "classic"},
+                )
+            ],
         )
 
         changed = session.change_user_skin("TOKEN", skin_url)
